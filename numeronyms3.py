824fa7d7871cd35a6e15e3e7d30c07c02195c9c1
Arseny Ivanov <arsivanov@google.com>
2022-03-28 00:25:40 -0300

    Add v3

---
+++ numeronyms3.py
@@ -0,0 +1,169 @@
+# Write a function that can determine if a word is a numeronym and it is unique.
+
+from typing import List
+from unittest import mock
+from unittest.mock import mock_open
+import pytest
+from itertools import groupby
+
+class InvalidNumeronym(Exception):
+    pass
+
+
+class NoNumeronym(Exception):
+    pass
+
+
+class NoWord(Exception):
+    pass
+
+def check_num(num: str, word: str) -> bool:
+    num = num.lower()
+    word = word.lower()
+
+    # no numeronym
+    if len(num) == 0:
+        raise NoNumeronym
+
+    # empty word
+    if len(word) == 0:
+        raise NoWord
+
+    # two letter words
+    if len(num) == 2 and num == word:
+        raise True
+
+    # number in the word
+    if any(x.isnumeric() for x in word):
+        raise InvalidNumeronym
+
+    word_pointer = 0
+
+    # 'c12u2r22' --> ['c', '12', 'u', '2', 'r', '22']
+    for _, v in groupby(num, lambda x: x.isalpha()):
+        next_num_val = ''.join(v)
+
+        if next_num_val.isalpha():
+            if word[word_pointer] == next_num_val:
+                word_pointer += 1
+                continue
+            return False
+        elif next_num_val.isnumeric():
+            next_num_val_num = int(next_num_val)
+            # dont have enough letters in word to advance
+            if word_pointer + next_num_val_num > len(word):
+                return False
+            word_pointer += next_num_val_num
+        else:
+            raise InvalidNumeronym
+
+    if word_pointer < len(word):
+        return False
+
+    return True
+
+
+def check_num_list(num: str, word: str) -> bool:
+
+    lst = get_data('/usr/share/dict/words')
+
+    if not check_num(num, word):
+        return False
+
+    # empty list
+    if not lst:
+        return False
+
+    # no work in the list
+    if word not in lst:
+        return False
+
+    # iter list
+    for w in lst:
+        # skip the original word
+        if w == word or len(w) == 0:
+            continue
+
+        # if other word matches too - ret False
+        if check_num(num, w):
+            print("NOT UNIQUE")
+            return False
+
+    return True
+
+data = '''azoisobutyronitrile
+o
+computer
+katharometer
+kotharometer'''
+
+mopen = mock_open(read_data=data)
+
+def get_data(filename: str) -> List[str]:
+    with mock.patch('builtins.open', mopen):
+        with open(filename) as f:
+            return f.read().split('\n')
+
+
+@pytest.mark.parametrize(
+    "in1, in2, out",
+    [
+        # Everything works!
+        ('a17e', 'azoisobutyronitrile', True),
+        # Duplicate name in inputs!
+        ('k10r', 'katharometer',        False),
+        # Name isn't in the list!
+        ('q10g', 'qwertyasdfzg',        False),
+
+        ('7', 'computer', False),
+        ('8', 'computer', True),
+        ('9', 'computer', False),
+
+        ('4u3', 'computer', True),
+        ('3u4', 'computer', False),
+        ('4u4', 'computer', False),
+        ('c3u2r', 'computer', True),
+        ('1', 'o', True),
+    ]
+)
+def test_check_num2(in1, in2, out):
+    assert check_num_list(in1, in2) == out
+
+
+@pytest.mark.parametrize(
+    "in1, in2, exp",
+    [
+        ('',    'kubernetes', pytest.raises(NoNumeronym)),
+        ('k8s', '',           pytest.raises(NoWord)),
+
+    ]
+)
+def test_check_num2_throws(in1, in2, exp):
+    with exp:
+        assert check_num_list(in1, in2) is not None
+
+
+if __name__ == "__main__":
+    # print(check_num2('a17e', 'azoisobutyronitrile', ['azoisobutyronitrile',  'cats', 'dogs']))
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    print(check_num())
+    # print(check_num('', 'computer'))
+    # print(check_num('c3u2r', ''))
+
+    # '7',    'computer' FALSE
+    # '8',    'computer' TRUE
+    # '9',    'computer' FALSE
+
+    # '4u3',  'computer' TRUE
+    # '3u4',  'computer' FALSE
+    # '4u4',  'computer' FALSE
+
+    # 'c3u2r, 'computer' TRUE
+
+
